0729d2e67dbeb9a0c8a91c1052794603ae11f312
Micah Evans <micah.s.evans@gmail.com>
2020-01-18 23:52:48 -0500

    Final commit before turning in the project

--- script.js
+++ script.js
@@ -84,31 +84,71 @@ $(document).ready(function () {
 
                     // Call to get Day 1 icon
                     var IconCodeDay1 = response.list[0].weather[0].icon;
-                    var day1IconURL = "http://openweathermap.org/img/wn/" + IconCodeDay1  + ".png";
+                    var day1IconURL = "http://openweathermap.org/img/wn/" + IconCodeDay1 + ".png";
                     $("#day1Img").attr("src", day1IconURL);
                     console.log(IconCodeDay1);
                     // Call to get Day 2 icon
                     var IconCodeDay2 = response.list[1].weather[0].icon;
-                    var day2IconURL = "http://openweathermap.org/img/wn/" + IconCodeDay2  + ".png";
+                    var day2IconURL = "http://openweathermap.org/img/wn/" + IconCodeDay2 + ".png";
                     $("#day2Img").attr("src", day2IconURL);
                     console.log(IconCodeDay2);
                     // Call to get Day 3 icon
                     var IconCodeDay3 = response.list[2].weather[0].icon;
-                    var day3IconURL = "http://openweathermap.org/img/wn/" + IconCodeDay3  + ".png";
+                    var day3IconURL = "http://openweathermap.org/img/wn/" + IconCodeDay3 + ".png";
                     $("#day3Img").attr("src", day3IconURL);
                     console.log(IconCodeDay3);
                     // Call to get Day 4 icon
                     var IconCodeDay4 = response.list[3].weather[0].icon;
-                    var day4IconURL = "http://openweathermap.org/img/wn/" + IconCodeDay4  + ".png";
+                    var day4IconURL = "http://openweathermap.org/img/wn/" + IconCodeDay4 + ".png";
                     $("#day4Img").attr("src", day4IconURL);
                     console.log(IconCodeDay4);
                     // Call to get Day 5 icon
                     var IconCodeDay5 = response.list[4].weather[0].icon;
-                    var day5IconURL = "http://openweathermap.org/img/wn/" + IconCodeDay5  + ".png";
+                    var day5IconURL = "http://openweathermap.org/img/wn/" + IconCodeDay5 + ".png";
                     $("#day5Img").attr("src", day5IconURL);
                     console.log(IconCodeDay5);
 
-                    
+                    // Call for Day 1 temp & the math to convert it from Kelvin to F
+                    var tempDay1 = response.list[0].main.temp;
+                    var tempDay1Math = (Math.ceil(tempDay1 - 273.15) * 9 / 5 + 32);
+                    console.log(tempDay1Math);
+                    $("#day1Temp").append(tempDay1Math);
+                    // Call for Day 2 temp & the math to convert it from Kelvin to F
+                    var tempDay2 = response.list[1].main.temp;
+                    var tempDay2Math = (Math.ceil(tempDay2 - 273.15) * 9 / 5 + 32);
+                    console.log(tempDay2Math);
+                    $("#day2Temp").append(tempDay2Math);
+                    // Call for Day 1 temp & the math to convert it from Kelvin to F
+                    var tempDay3 = response.list[2].main.temp;
+                    var tempDay3Math = (Math.ceil(tempDay3 - 273.15) * 9 / 5 + 32);
+                    console.log(tempDay3Math);
+                    $("#day3Temp").append(tempDay3Math);
+                    // Call for Day 1 temp & the math to convert it from Kelvin to F
+                    var tempDay4 = response.list[3].main.temp;
+                    var tempDay4Math = (Math.ceil(tempDay4 - 273.15) * 9 / 5 + 32);
+                    console.log(tempDay4Math);
+                    $("#day4Temp").append(tempDay4Math);
+                    // Call for Day 1 temp & the math to convert it from Kelvin to F
+                    var tempDay5 = response.list[4].main.temp;
+                    var tempDay5Math = (Math.ceil(tempDay5 - 273.15) * 9 / 5 + 32);
+                    console.log(tempDay5Math);
+                    $("#day5Temp").append(tempDay5Math);
+
+                    // Humidity
+                    var humDay1 = response.list[0].main.humidity;
+                    $("#day1Hum").append(humDay1);
+                    var humDay2 = response.list[1].main.humidity;
+                    $("#day2Hum").append(humDay2);
+                    var humDay3 = response.list[2].main.humidity;
+                    $("#day3Hum").append(humDay3);
+                    var humDay4 = response.list[3].main.humidity;
+                    $("#day4Hum").append(humDay4);
+                    var humDay5 = response.list[4].main.humidity;
+                    $("#day5Hum").append(humDay5);
+
+
+
+
 
 
                 })
